6688b2f7b9e78cac3b291c38c09b3f89f659a4f1
riyaz <syedriyaz0@gmail.com>
2020-11-18 21:10:27 +0530

    commit

---
+++ SeleniumWeb/src/test/java/com/pageObjectModel/PageObjectModel.java
@@ -0,0 +1,15 @@
+package com.pageObjectModel;
+
+import org.openqa.selenium.By;
+
+public class PageObjectModel {
+
+	
+	//page object model- object are define with "By locator" while creating objects
+	
+	public By contact = By.xpath("");
+	
+	
+	
+	
+}
